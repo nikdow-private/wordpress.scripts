94c8bac5a7e551454117a060bc77ce26020c07d4
nikdow <support@cbdweb.net>
2026-03-31 10:49:02 +1100

    add gitgub private key to cron_gitpush.bash;

--- cron_gitpush.bash
+++ cron_gitpush.bash
@@ -3,4 +3,4 @@
 cd /etc
 git add --all
 git commit -a -m `date +"%Y-%m-%d-%H:%M:%S"`
-git push https://github.com/nikdow-private/nikdow_etc.git master
+git push https://nikdow-private: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@github.com/nikdow-private/nikdow_etc.git master
